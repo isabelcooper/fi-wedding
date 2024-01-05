d22da0262c447b8cb614d5d2a36840df5ed538fa
isabelcooper <isabelcooper093@gmail.com>
2024-01-05 09:08:54 +0000

    Add image banner

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,12 @@ import React, { useState } from 'react';
 import './App.css';
 import theme from "./theme";
 
-import { Box, Button, Container, Stack, Tab, Tabs, Typography } from '@mui/material';
+import { Box, Button, Container, ImageList, ImageListItem, Stack, Tab, Tabs, Typography } from '@mui/material';
 import { ThemeProvider, styled } from "@mui/material/styles";
+import christmas from './assets/christmas.jpeg'
+import engagement from './assets/engagement.jpeg'
+import skiing from './assets/skiing.jpeg'
+import summer from './assets/summer.jpeg'
 
 import Day from "./tabs/day"
 
@@ -56,14 +60,40 @@ function App() {
             <Typography variant="subtitle"> Registry</Typography>
             <Typography>Buy us things</Typography>
 
-          </Stack>
-        )}
-        {currentTab === "rsvp" && (
-          <Stack spacing={2} margin="32px">
-            <Typography variant="subtitle">RSVP</Typography>
-
-          </Stack>
-        )}
+        <ImageList sx={{ width: "100%", height: 450 }} cols={4} rowHeight={164}>
+          <ImageListItem key={"summer"}>
+            <img
+              srcSet={`${summer}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+              src={`${summer}?w=164&h=164&fit=crop&auto=format`}
+              alt="summer"
+              loading="lazy"
+            />
+          </ImageListItem>
+          <ImageListItem key={"skiing"}>
+            <img
+              srcSet={`${skiing}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+              src={`${skiing}?w=164&h=164&fit=crop&auto=format`}
+              alt="skiing"
+              loading="lazy"
+            />
+          </ImageListItem>
+          <ImageListItem key={"engagement"}>
+            <img
+              srcSet={`${engagement}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+              src={`${engagement}?w=164&h=164&fit=crop&auto=format`}
+              alt="engagement"
+              loading="lazy"
+            />
+          </ImageListItem>
+          <ImageListItem key={"christmas"}>
+            <img
+              srcSet={`${christmas}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+              src={`${christmas}?w=164&h=164&fit=crop&auto=format`}
+              alt="christmas"
+              loading="lazy"
+            />
+          </ImageListItem>
+        </ImageList>
       </Container>
     </ThemeProvider>
   )
